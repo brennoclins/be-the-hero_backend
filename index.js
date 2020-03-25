19d4ab3aec37d3c20b733dda493f4212b5189608
Brenno Dev <dev@bcl-st.com.br>
2020-03-25 08:08:42 -0300

    Aula 2:  informações sobre os metodos HTTP

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Metodos HTTP
+ * 
+ * GET: Busca uma informação do back-end
+ * POST: Cria uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
 app.get('/', (request, response) => {
-    return response.send('Hello World Brenno!');
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Brenno C. Lins',
+        mensagem:'Estudando na quarentena'
+    });
 });
 
 app.listen(3333);

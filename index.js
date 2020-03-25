88d0f11d7f52cfff955b44aa3b73a320971931b2
Brenno Dev <dev@bcl-st.com.br>
2020-03-25 08:27:11 -0300

    Tipos de parametros, acessando informações com o QUERY PARAMS

--- index.js
+++ index.js
@@ -11,11 +11,29 @@ const app = express();
  * DELETE: Deletar uma informação no back-end
  */
 
-app.get('/', (request, response) => {
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: 
+ */
+
+app.get('/users', (request, response) => {
+    const params = request.query;
+    console.log(params);
+
     return response.json({
-        evento: 'Semana OmniStack 11.0',
+        evento: 'Ola! Semana OmniStack 11.0',
         aluno: 'Brenno C. Lins',
         mensagem:'Estudando na quarentena'
+    });
+});
+
+app.post('/users', (request, response) => {
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Brenno C. Lins'
     });
 });
 

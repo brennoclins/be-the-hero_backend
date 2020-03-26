c30bbb36319c4fd7dab2047189da93fd900a7295
Brenno Dev <dev@bcl-st.com.br>
2020-03-26 02:02:15 -0300

    Request Body

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+//informar ao express para usar o JSON
+app.use(express.json());
+
 /**
  * Metodos HTTP
  * 
@@ -16,21 +19,24 @@ const app = express();
  * 
  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
  * Route Params: Parâmetros utilizados para identificar recursos
- * Request Body: 
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
  */
 
-app.get('/users', (request, response) => {
-    const params = request.query;
+app.get('/users/:id', (request, response) => {
+    const params = request.params;
     console.log(params);
 
     return response.json({
         evento: 'Ola! Semana OmniStack 11.0',
         aluno: 'Brenno C. Lins',
-        mensagem:'Estudando na quarentena'
+        mensagem:'Route Params'
     });
 });
 
 app.post('/users', (request, response) => {
+    const body = request.body;
+    console.log(body);
+
     return response.json({
         evento: 'Semana OmniStack 11.0',
         aluno: 'Brenno C. Lins'

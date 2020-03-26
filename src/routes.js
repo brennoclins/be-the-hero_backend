2db092770a4e6403ffadc82a21ee8518ae7d3616
Brenno Dev <dev@bcl-st.com.br>
2020-03-26 18:35:16 -0300

    criando uma rota para lista todos os registro na tabela ongs

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,13 @@ const connection = require('./database/connection');
 
 const routes = express.Router();
 
+//lista registro da tabela ongs
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+
+    return response.json(ongs);
+});
+
 //rota para criar as ongs
 routes.post('/ongs', async (request, response) => {
     const { name, email, whatsapp, city, uf } = request.body;
